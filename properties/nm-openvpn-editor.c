a2cc2ac7f9a1f480f99f20e300e6e2fa5980651e
FILICIAK Piotr <Piotr.FILICIAK@ext.ec.europa.eu>
2026-05-21 19:23:37 +0200

    code refactor

--- properties/nm-openvpn-editor.c
+++ properties/nm-openvpn-editor.c
@@ -237,6 +237,33 @@ tls_setup (GtkBuilder *builder,
 	g_signal_connect_object (cert, "changed", G_CALLBACK (tls_cert_changed_cb), ca_chooser, 0);
 }
 
+static char *
+pkcs11_cert_label (pkcs11h_certificate_id_t cert_id)
+{
+	static const char hex_chars[] = "0123456789ABCDEF";
+	char *label = NULL;
+	gs_free char *hex = NULL;
+	size_t j, len;
+
+	len = cert_id->attrCKA_ID_size * 2;
+	hex = g_malloc (len + 1);
+	for (j = 0; j < cert_id->attrCKA_ID_size; j++) {
+		unsigned char b = cert_id->attrCKA_ID[j];
+		hex[j * 2]     = hex_chars[b >> 4];
+		hex[j * 2 + 1] = hex_chars[b & 0x0F];
+	}
+	hex[len] = '\0';
+
+	if (cert_id->token_id && cert_id->token_id->label[0] && len)
+		label = g_strdup_printf ("%s (%s)", cert_id->token_id->label, hex);
+	else if (cert_id->token_id && cert_id->token_id->label[0])
+		label = g_strdup (cert_id->token_id->label);
+	else if (len)
+		label = g_steal_pointer (&hex);
+
+	return label;
+}
+
 static void
 pkcs11_populate_ids_for_provider (GtkComboBoxText *id_combo, const char *provider_path)
 {
@@ -266,8 +293,6 @@ pkcs11_populate_ids_for_provider (GtkComboBoxText *id_combo, const char *provide
 			gs_free char *ser = NULL;
 			gs_free char *label = NULL;
 			size_t ser_len = 0;
-			GString *hex;
-			size_t j;
 
 			if (pkcs11h_certificate_serializeCertificateId (
 			        NULL, &ser_len, cur->certificate_id) != CKR_OK)
@@ -277,21 +302,9 @@ pkcs11_populate_ids_for_provider (GtkComboBoxText *id_combo, const char *provide
 			        ser, &ser_len, cur->certificate_id) != CKR_OK)
 				continue;
 
-			hex = g_string_new (NULL);
-			for (j = 0; j < cur->certificate_id->attrCKA_ID_size; j++)
-				g_string_append_printf (hex, "%02X", (unsigned char) cur->certificate_id->attrCKA_ID[j]);
+			label = pkcs11_cert_label (cur->certificate_id);
 
-			if (cur->certificate_id->token_id->label[0] && hex->len)
-				label = g_strdup_printf ("%s (%s)", cur->certificate_id->token_id->label, hex->str);
-			else if (cur->certificate_id->token_id->label[0])
-				label = g_strdup (cur->certificate_id->token_id->label);
-			else if (hex->len)
-				label = g_strdup (hex->str);
-			else
-				label = g_strdup (ser);
-			g_string_free (hex, TRUE);
-
-			gtk_combo_box_text_append (id_combo, ser, label);
+			gtk_combo_box_text_append (id_combo, ser, label ? label : ser);
 		}
 		pkcs11h_certificate_freeCertificateIdList (certs);
 	}
@@ -406,22 +419,9 @@ pkcs11_setup (GtkBuilder *builder,
 				if (pkcs11h_initialize () == CKR_OK) {
 					if (pkcs11h_certificate_deserializeCertificateId (&cert_id, id) == CKR_OK
 					    && cert_id) {
-						GString *hex = g_string_new (NULL);
-						size_t j;
-
-						for (j = 0; j < cert_id->attrCKA_ID_size; j++)
-							g_string_append_printf (hex, "%02X", (unsigned char) cert_id->attrCKA_ID[j]);
-
-						if (cert_id->token_id && cert_id->token_id->label[0] && hex->len)
-							display = g_strdup_printf ("%s (%s)", cert_id->token_id->label, hex->str);
-						else if (cert_id->token_id && cert_id->token_id->label[0])
-							display = g_strdup (cert_id->token_id->label);
-						else if (hex->len)
-							display = g_strdup (hex->str);
-						g_string_free (hex, TRUE);
-					}
-					if (cert_id)
+						display = pkcs11_cert_label (cert_id);
 						pkcs11h_certificate_freeCertificateId (cert_id);
+					}
 					pkcs11h_terminate ();
 				}
 
